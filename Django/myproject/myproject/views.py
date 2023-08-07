702563e035c7a8d1e9820c03de9ad41b05f04255
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-08-08 00:13:16 +0530

    updated

--- Django/myproject/myproject/views.py
+++ Django/myproject/myproject/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def homePage(request):
-    dict = {
-        'list':['Himanshu','Susheel','Aman','Ankit']
-    }
+    # dict = {
+    #     'list':['Himanshu','Susheel','Aman','Ankit']
+    # }
+    dict = {}
+    try:
+        if request.method == 'POST':
+            num1 = int(request.POST.get('num1'))
+            num2 = int(request.POST.get('num2'))
+            sum = num1+num2
+            dict={
+                'n1':num1,
+                'n2':num2,
+                'sum':sum
+            }
+    except:
+        pass        
     return render(request,"index.html",dict)
 def hello_world(request):
     return HttpResponse("<h1>Hello, World!</h1>")
