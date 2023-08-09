c5a519b6a6c02933a1fb3ac2256daf0ec62c0082
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-08-09 13:16:16 +0530

    for deployement

--- Django-projects/myProject/crudproject/crudapp/urls.py
+++ Django-projects/myProject/crudproject/crudapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name='create'),
+    path('', views.read, name='read'),
     path('read/', views.read, name='read'),
     path('update/', views.update, name='update'),
     path('delete/', views.delete, name='delete'),
