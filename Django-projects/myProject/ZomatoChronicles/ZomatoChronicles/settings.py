86c65b3f235c717666897ab32890470af994798a
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-08-11 15:01:33 +0530

    mongodb files

--- Django-projects/myProject/ZomatoChronicles/ZomatoChronicles/settings.py
+++ Django-projects/myProject/ZomatoChronicles/ZomatoChronicles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oreh4q3xgh2t$a14ysv1*-hq1^vbg7$q1s_kqno3&w*bj12dcu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'zomato'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'zomato', 'templates')],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Django-projects/myProject/crudproject/crudproject/settings.py
+++ Django-projects/myProject/crudproject/crudproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r%u!pz48)kpopldq8a3krl06a&ml2ubewr!a=hkh3bp8lejel#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
